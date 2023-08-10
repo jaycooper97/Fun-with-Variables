ef8705361ff17b547703d37412ed3ec8b0830b57
jay cooper <15jacooper@gmail.com>
2023-08-09 21:41:50 -0400

    new push

---
+++ index.js
@@ -0,0 +1,28 @@
+var peach = 8
+var mango = 26
+var apple = 40
+var orange = 12
+var starFruit = 32
+var banana = 66
+var grape = 2
+var avacado = 16
+var cranberry = 90
+var nectarine = 44
+
+console.log(grape + nectarine);
+console.log(apple + mango);
+
+console.log(starFruit - mango);
+console.log(apple - banana);
+
+console.log(nectarine * starFruit);
+console.log(avacado * cranberry);
+
+console.log(cranberry / nectarine);
+console.log(orange / grape);
+
+apple++
+peach++
+
+starFruit--
+cranberry--
